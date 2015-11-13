0cf08a560a922ba3848dd05e6dbb8fade224fc50
jdwright555 <jdwright555@gmail.com>
2015-11-13 15:48:12 +0000

    added code to read data from mongo and plot

--- dev/front-end/app/controllers/resultsCtrl.js
+++ dev/front-end/app/controllers/resultsCtrl.js
@@ -18,7 +18,36 @@ angular.module('resultsCtrl', ['resultsService'])
 
             // bind the users that come back to vm.users
             vm.users = data;
+
+            // grabs data for plot, first as strings
+            var names = "";
+            var ages = "";
+
+            for (var i = 0; i < data.length; i++) {
+                var name_i = data[i].fullname;
+                var age_i = data[i].age;
+                if (i != data.length - 1) {
+                    names = names + name_i + ",";
+                    ages = ages + age_i + ","; // comma separated strings can be converted to numeric arrays
+                } else {
+                    names = names + name_i;
+                    ages = ages + age_i;
+                };
+            };
+            // converts age string to numeric array, and parses string array
+            ages = ages.split(',').map(Number);
+            names = names.split(',');
+
+            var dat = [{
+                x: names,
+                y: ages,
+                type: 'bar'
+            }];
+
+            Plotly.newPlot('plot_area', dat);
+
         });
+
 
     // run analysis script
     vm.analyse = function() {
